1bf5e28fb3de33af098ce49398dbc66bf8512065
Jamie Hale <jamiesonhale@gmail.com>
2018-01-24 20:25:28 -0500

    Add router

--- src/main.js
+++ src/main.js
@@ -4,12 +4,34 @@ import Farkle from './Farkle.vue'
 import '!vue-style-loader!css-loader!tachyons/css/tachyons.css'
 
 import VueJsModal from 'vue-js-modal'
+import VueRouter from 'vue-router'
 
 Vue.use(VueJsModal, {
   dialog: true
 })
 
+Vue.use(VueRouter);
+
+import GameSetup from './GameSetup.vue';
+import Game from './Game.vue';
+
+const routes = [
+  {
+    path: '/setup',
+    component: GameSetup
+  },
+  {
+    path: '/',
+    component: Game
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
 new Vue({
+  router,
   el: '#app',
   components: {
     'farkle': Farkle
